3dfa5555ebba5deaea29a7f62577d42ff3f7cdf9
Matthew Merrill <git@mattmerr.com>
2022-09-21 22:31:21 -0700

    Update Dockerfile for 19, hide beets_channels

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run --detach -e BEETS_ALLOWED_CHANNEL=439292332144066560 -v /srv/beets/prod.secret:/run/beets_discord_token -v /srv/beets/beets.sqlite:/app/beets.sqlite:rw --restart=always -it beets
+#!/bin/sh
+docker run --detach -e BEETS_ALLOWED_CHANNEL=$(sh beets_channels.sh) -v /srv/beets/prod.secret:/run/beets_discord_token -v /srv/beets/beets.sqlite:/app/beets.sqlite:rw --restart=always -it --name=beets beets
